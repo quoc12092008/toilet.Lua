4278d0079de9ce165f4b25986f0dba81fd398028
quoc12092008 <113664115+quoc12092008@users.noreply.github.com>
2024-03-03 16:12:23 +0700

    Update toiletbanana.lua

--- toiletbanana.lua
+++ toiletbanana.lua
@@ -1,16 +1,3 @@
-local Players = game:GetService("Players")
-
--- Tọa độ cần kiểm tra khoảng cách
-local targetPosition = Vector3.new(1026.103759765625, -33.44594955444336, 3740.22998046875)
-
--- Khoảng cách tối đa cho phép người chơi đến trước khi bị kick ra (đo lường bằng khoảng cách Euclidean)
-local maxDistance = 5
-
--- Hàm tính khoảng cách giữa hai điểm
-local function calculateDistance(point1, point2)
-    return (point1 - point2).magnitude
-end
-
 -- Hàm kiểm tra và kick người chơi ra khi ở gần tọa độ cần kiểm tra
 local function checkDistanceAndKick(player)
     local character = player.Character
@@ -28,11 +15,6 @@ local function checkDistanceAndKick(player)
     else
         warn(player.Name .. " chưa tham gia trò chơi")
     end
-end
-
--- Lặp qua tất cả người chơi trong trò chơi và kiểm tra khoảng cách
-for _, player in ipairs(Players:GetPlayers()) do
-    checkDistanceAndKick(player)
 end
 
 -- Sự kiện khi một người chơi tham gia trò chơi
