c8bb334651e8897249a5f19e023c4044269fae22
quoc12092008 <113664115+quoc12092008@users.noreply.github.com>
2024-03-27 01:19:01 +0700

    Update toiletbanana.lua

--- toiletbanana.lua
+++ toiletbanana.lua
@@ -4,13 +4,6 @@ local postOfficePosition = Vector3.new(-457, 250, -67)
 player.Character:SetPrimaryPartCFrame(CFrame.new(postOfficePosition))
 print("Bạn đã đến post office!")
 
-getgenv().config = {
-    sendMail = true,
-    userToMail = "petkhoroblox",
-    message = "1000 gems",
-    gemAmount = 1000,
-}
-
 local Config = getgenv().config
 
 function autoMail()
@@ -23,25 +16,29 @@ function autoMail()
 
         sendPackages.YourGems.GemAmount.GemAmount:CaptureFocus()
         sendPackages.YourGems.GemAmount.GemAmount.Text = tostring(Config.gemAmount)
-
-        local sendButton = {
-            [1] = {
-                [1] = {
-                    [1] = "3035333364313331633135613432396538343536643366316231376634663538",
-                    [2] = 2891179472,
-                    [3] = "Troops",
-                    [4] = "0ebec1186c134ceda392",
-                    [5] = 0,
-                    [6] = "cccccccc"
-                },
-                [2] = "mL"
-            }
-        }
-        
-        game:GetService("ReplicatedStorage"):WaitForChild("dataRemoteEvent"):FireServer(unpack(args))
-        wait(0.5)
-            Config.sendMail = false
     end
 end
 
+function sendbutton()
+    local args = {
+        [1] = {
+            [1] = {
+                [1] = "3035333364313331633135613432396538343536643366316231376634663538",
+                [2] = 2891179472,
+                [3] = "Troops",
+                [4] = "0ebec1186c134ceda392",
+                [5] = 0,
+                [6] = "cccccccc"
+            },
+            [2] = "mL"
+        }
+    }
+        
+    game:GetService("ReplicatedStorage"):WaitForChild("dataRemoteEvent"):FireServer(unpack(args))
+end
+
+wait(0.5)
+Config.sendMail = false
+
 autoMail()
+sendbutton()
