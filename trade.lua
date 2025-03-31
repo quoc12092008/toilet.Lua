8228f2d73276997cbc8fb9f822ab85a705cad17e
tbikhung <113664115+quoc12092008@users.noreply.github.com>
2025-03-31 21:46:05 +0700

    Update trade.lua

--- trade.lua
+++ trade.lua
@@ -1,4 +1,16 @@
-getgenv().Config = {
-    ["AreaToFarm"] = 7
+script_key = "KeGmGoyFhvzluklNUmCNzGYZhrugqjFs";
+_G.GDO_MINING_EVENT = true
+_G.GBUY_DIAMOND_PICKAXE = false
+_G.GEVENT_UPGRADES = {
+    "MiningEventLessPetsRequiredToComboThem",
+    "MiningEventIncreaseTierUpChance",
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8b810254a48d29099a962e99b2291c45.lua"))()
+_G.GMINING_EVENT_HATCH_PET_CHANCE = 90
+_G.GGFX_MODE = 1
+_G.GMINING_EVENT_ZONE_TO = 8
+_G.GEVENT_FPS  = 60
+_G.GZONE_TO = 1
+_G.GMINING_EVENT_MAX_AUTO_CRAFT_RECIPE = 8
+_G.GWEBHOOK_USERID = "918007508935802910"
+_G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1327721643283583028/kKC9C_G16vF8Qarm7x-J4gkemuV0zfvKO3eRwSWDDvAvCC06BPczRoRVKUQTsRgGvjvm"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
